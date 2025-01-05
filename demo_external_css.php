70d82694c09207d5dfdaefb62a434203dda19310
Orange Cat <deepnikhare2006@gmail.com>
2025-01-06 01:40:02 +0530

    Add files via upload

---
+++ demo_external_css.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>History of computer science</h1>
+
+    <div class="hello">
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Glen_Beck_and_Betty_Snyder_program_the_ENIAC_in_building_328_at_the_Ballistic_Research_Laboratory.jpg/375px-Glen_Beck_and_Betty_Snyder_program_the_ENIAC_in_building_328_at_the_Ballistic_Research_Laboratory.jpg" alt="yoo">    
+
+        <p>
+          <a href="https://en.wikipedia.org/wiki/Computer_science">History of computer science</a> Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi totam veritatis natus nostrum error, unde eligendi modi delectus vero, earum nesciunt, sequi pariatur numquam inventore ipsum? Nihil beatae laudantium vitae eligendi nam ratione reiciendis sunt nobis corrupti, voluptas debitis aut doloremque tempore saepe. Nemo neque fugiat fugit, doloribus eligendi praesentium, repellendus sit, ducimus numquam consequuntur odit debitis asperiores itaque ex quia reiciendis impedit nam quam totam ad suscipit corrupti animi. Omnis dignissimos hic, ratione aliquam tempora eveniet obcaecati, voluptatibus nemo dolorem fugiat autem molestias nostrum saepe nam, porro pariatur veritatis voluptate odio dicta dolorum. Nostrum voluptatum autem eveniet rem velit! visit: <a href="https://www.wikipedia.org" target="_blank">Wikipedia</a>
+        </p>
+
+    </div>
+
+
+    <div class="yo">
+
+        
+        <p id = "p1">
+        <a href="https://en.wikipedia.org/wiki/Gottfried_Wilhelm_Leibniz">Gottfried Wilhelm Leibniz (1646–1716)</a> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt neque dolorem, accusantium, est impedit consequatur aliquam delectus, ipsa non quas illo ipsam rerum voluptatem officiis sed nulla. Expedita vero ab magnam dignissimos, eligendi provident voluptatem minima distinctio qui impedit fugiat hic in, eius praesentium vel architecto modi esse eos. Doloremque autem quidem ex aliquam dolore maiores, neque quia ipsum architecto ipsam ut eos fugiat aut eum odio! Vel, nulla quae reprehenderit quas eos natus nesciunt expedita, ipsa adipisci eius perferendis voluptates harum facere ipsum est. Iste consequatur beatae eius assumenda voluptatem delectus deleniti possimus illum, qui ipsum. Consectetur, veritatis vero! visit: <a href="https://www.wikipedia.org" target="_blank">Wikipedia</a>
+        </p>
+        
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Christoph_Bernhard_Francke_-_Bildnis_des_Philosophen_Leibniz_%28ca._1695%29.jpg/243px-Christoph_Bernhard_Francke_-_Bildnis_des_Philosophen_Leibniz_%28ca._1695%29.jpg" alt="image">
+        
+    </div>
+
+    <div class="yo">
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Ada_lovelace.jpg/255px-Ada_lovelace.jpg" alt="image">
+        
+        <p id = "p1">
+           <a href="https://en.wikipedia.org/wiki/Ada_Lovelace">Ada Lovelace</a> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt neque dolorem, accusantium, est impedit consequatur aliquam delectus, ipsa non quas illo ipsam rerum voluptatem officiis sed nulla. Expedita vero ab magnam dignissimos, eligendi provident voluptatem minima distinctio qui impedit fugiat hic in, eius praesentium vel architecto modi esse eos. Doloremque autem quidem ex aliquam dolore maiores, neque quia ipsum architecto ipsam ut eos fugiat aut eum odio! Vel, nulla quae reprehenderit quas eos natus nesciunt expedita, ipsa adipisci eius perferendis voluptates harum facere ipsum est. Iste consequatur beatae eius assumenda voluptatem delectus deleniti possimus illum, qui ipsum. Consectetur, veritatis vero! visit: <a href="https://www.wikipedia.org" target="_blank">Wikipedia</a>
+        </p>
+        
+        
+    </div>
+
+    <div class="hello">
+
+        <p id = "p1">
+
+         <a href="https://en.wikipedia.org/wiki/Charles_Babbage">Charles Babbage</a> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt neque dolorem, accusantium, est impedit consequatur aliquam delectus, ipsa non quas illo ipsam rerum voluptatem officiis sed nulla. Expedita vero ab magnam dignissimos, eligendi provident voluptatem minima distinctio qui impedit fugiat hic in, eius praesentium vel architecto modi esse eos. Doloremque autem quidem ex aliquam dolore maiores, neque quia ipsum architecto ipsam ut eos fugiat aut eum odio! Vel, nulla quae reprehenderit quas eos natus nesciunt expedita, ipsa adipisci eius perferendis voluptates harum facere ipsum est. Iste consequatur beatae eius assumenda voluptatem delectus deleniti possimus illum, qui ipsum. Consectetur, veritatis vero! visit: <a href="https://www.wikipedia.org" target="_blank">Wikipedia</a>
+       
+        </p>
+        
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Babbage40.png/255px-Babbage40.png" alt="image">
+        
+    </div>
+
+</body>
+</html>
---
+++ demo_inline_css.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p style = "color: blue; font-size: 40px;">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sed nisi dolore, maxime eligendi porro aliquam quae ut quisquam ducimus rerum, veritatis sunt. Cumque hic nisi rem dolores pariatur, accusamus totam repudiandae ea officiis quasi culpa error tempore ratione sint, quas alias iusto magnam tenetur neque esse optio in minima.
+    </p>
+
+    <p style = "color: gray; font-size: 40px;">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, sed nisi dolore, maxime eligendi porro aliquam quae ut quisquam ducimus rerum, veritatis sunt. Cumque hic nisi rem dolores pariatur, accusamus totam repudiandae ea officiis quasi culpa error tempore ratione sint, quas alias iusto magnam tenetur neque esse optio in minima.
+    </p>
+</body>
+</html>
